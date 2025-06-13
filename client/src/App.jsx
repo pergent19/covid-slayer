edb4481c691f5ce8b417ed590f857ad6b223a3c0
unknown <pergent.galang19@gmail.com>
2025-06-13 13:11:21 +0800

    Fix app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/LoginPage';
 import Register from './pages/RegisterPage'
-import HomePage from './pages/HomePage';
 import GamePage from "./pages/GamePage";
-// import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 import Navbar from './components/Navbar';
 
